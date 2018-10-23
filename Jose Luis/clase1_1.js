c456b79241f209457bc010f268b3149a5e0070c6
Eduardo <mejiasavilaeduardo@gmail.com>
2018-10-23 13:43:31 -0300

    add hecho por jose luis

---
+++ Jose Luis/clase1_1.js
@@ -0,0 +1,10 @@
+var a = 10;
+var c, b = "jose luis rojas dellan";
+var d;
+var e;
+e = b;
+c = true;
+a = 50;
+d = false;
+alert("el monto ganado fue " + e);
+console.log("el valor de a es ", a, " y b ", b);
---
+++ Jose Luis/clase1_1.ts
@@ -0,0 +1,11 @@
+var a:number=10
+var c:boolean,b:string="jose luis rojas dellan"
+var d:any
+let e:string
+
+e=b
+c=true
+a=50
+d=false
+alert("el monto ganado fue "+e)
+console.log("el valor de a es ",a," y b ",b)
